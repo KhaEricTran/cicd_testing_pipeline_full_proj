260b7c112c212a24b1d626c93b45d5e4009b434c
riczfe <s3750945@student.rmit.edu.au>
2024-01-22 15:00:43 +0700

    test

--- ADVANCED-REQUIREMENTS/ansible/setupAnsible.sh
+++
@@ -1,5 +0,0 @@
-sudo yum update -y
-sudo amazon-linux-extras install ansible2 -y
-sudo yum install git -y
-git clone https://github.com/KhaEricTran/cicd_testing_pipeline_full_proj.git
-ansible-playbook -i hosts.ini jenkins_aws_ec2_playbook.yml
